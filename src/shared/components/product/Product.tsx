4e17b9ab9f5dcb69ab3708529ca5af3afbeede5e
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2022-10-06 18:57:02 -0300

    menu visual produto

--- src/shared/components/product/Product.tsx
+++ src/shared/components/product/Product.tsx
@@ -1,6 +1,8 @@
-import { Box, Grid, Typography } from "@mui/material"
+import { Box, Grid, IconButton, Typography } from "@mui/material"
 import { IProduct } from "../../service/api/products"
 import  place from "../../../assets/camisa.jpg"
+import { useState } from "react"
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 type TInfoProduct = IProduct &{
     key: string
@@ -11,6 +13,18 @@ export const Product: React.FC<TInfoProduct> = ({
     info,
     codeRFID
 }) => {
+
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    }
+    const handleClose = () => {
+        setAnchorEl(null);
+    }
+
+    //falta colocar coisa no menu e adicionar as funções
+
     return(
         <Box
         sx={{
@@ -26,10 +40,22 @@ export const Product: React.FC<TInfoProduct> = ({
             backgroundSize:'cover'
             }}>
             </Grid> 
-            <Box p={1}>
-                <Typography fontSize={'16px'} fontWeight={700} color={'#505050'}>{description}</Typography>
-                <Typography fontSize={'10px'} color={'#989898'}>{info}</Typography>
-                <Typography fontSize={'12px'} fontWeight={500} color={'#505050'}>{codeRFID}</Typography>
+            <Box p={1} height={'20%'} display={'flex'} alignItems={'center'} justifyContent={'space-between'}>
+                <Box>
+                    <Typography fontSize={'16px'} fontWeight={700} color={'#505050'}>{description}</Typography>
+                    <Typography fontSize={'10px'} color={'#989898'}>{info}</Typography>
+                    <Typography fontSize={'12px'} fontWeight={500} color={'#505050'}>{codeRFID}</Typography>
+                </Box>
+                <Box>
+                    <IconButton
+                    aria-controls={open ? 'long-menu' : undefined}
+                    aria-expanded={open ? 'true' : undefined}
+                    aria-haspopup="true"
+                    onClick={handleClick}
+                    >
+                        <MoreVertIcon />
+                    </IconButton>
+                </Box>
             </Box>
         </Box>
     )
