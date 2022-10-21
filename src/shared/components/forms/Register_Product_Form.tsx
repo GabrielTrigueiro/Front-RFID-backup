ffff91d335bad474684e6277f9edc99b7ee397d4
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2022-10-21 20:22:11 -0300

    visual da modal de cadastro de produto pronto?

--- src/shared/components/forms/Register_Product_Form.tsx
+++ src/shared/components/forms/Register_Product_Form.tsx
@@ -26,146 +26,153 @@ export const Register_Product_Form: React.FC<{
                     Cadastrar
                 </Box>
                 <Divider flexItem />
-                <Box className="form_box">
-                    <Box className="container da imagem e form"
-                        bgcolor={"#fff"}
-                        display={"flex"}
-                        flexDirection={"row"}
-                        alignItems={"center"}
+                <Box className="form_box"
+                    sx={{
+                        //bgcolor: "#990",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        flexDirection: "row",
+                        width:"100%"
+                }}
+                >
+                    <Box className="imagem"
+                        sx={{
+                            minWidth: {
+                                sm: 100,
+                                md: 300,
+                                xl: 300,
+                            },
+                            height: {
+                                sm: 100,
+                                md: 300,
+                                xl: 300,
+                            },
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            bgcolor: "#D9D9D9",
+                        }}
                     >
-                        <Box className="imagem"
-                            sx={{
-                                minWidth: {
-                                    sm: 100,
-                                    md: 300,
-                                    xl: 300,
-                                },
-                                height: {
-                                    sm: 100,
-                                    md: 300,
-                                    xl: 300,
-                                },
-                                display: "flex",
-                                alignItems: "center",
-                                justifyContent: "center",
-                                bgcolor:"#D9D9D9",
-                                margin:6
-                            }}
+                        <Button
+                            type="submit"
+                            variant="contained"
                         >
-                            <Button
-                                type="submit"
-                                variant="contained"
-                            >
-                                <UploadFileIcon />Upload
-                            </Button>
+                            <UploadFileIcon />Upload
+                        </Button>
 
-                        </Box>
-                        <Box
+                    </Box>
+                    <Box
                         sx={{
                             height: {
                                 sm: 100,
                                 md: 300,
                                 xl: 300,
                             },
-                            width:"100%",
+                            //bgcolor: "#920",
+                            display:"flex",
+                            justifyContent:"center"
                         }}
+                    >
+                        <Box className="form"
+                            sx={{
+                                width: "90%",
+                                height: {
+                                    sm: 100,
+                                    md: 300,
+                                    xl: 300,
+                                },
+                                display: "flex",
+                                flexDirection: "column",
+                                //bgcolor: "#000",
+                            }}
                         >
-                            <Box className="form"
-                                sx={{
-                                    width:"90%",
-                                    display:"flex",
-                                    flexDirection:"column",
-                                    padding:1
-                                }}
-                                // bgcolor={"#ccc"}
-                            >
-                                <FormControl size="small" className="input">
-                                    <InputLabel>Nome do Produto</InputLabel>
-                                    <FormInput
-                                        name="info"
-                                        type="text"
-                                        label="Nome do Produto"
-                                    />
-                                </FormControl>
+                            <FormControl sx={{ marginLeft: 1, marginRight: 1, marginBottom: 1, borderRadius: 1 }} size="small" className="input">
+                                <InputLabel>Nome do Produto</InputLabel>
+                                <FormInput
+                                    name="info"
+                                    type="text"
+                                    label="Nome do Produto"
+                                />
+                            </FormControl>
 
-                                <Box sx={{display:"flex"}}>
-                                    <Box sx={{display:"flex", flexDirection:"column"}}>
-                                        <FormControl size="small" className="input">
-                                            <InputLabel>Quantidade</InputLabel>
-                                            <FormInput
-                                                name="Quantidade"
-                                                type="text"
-                                                label="Quantidade"
-                                            />
-                                        </FormControl>
+                            <Box sx={{ display: "flex", justifyContent:"space-between"}}>
+                                <Box sx={{ display: "flex", flexDirection: "column", width:"50%"}}>
+                                    <FormControl sx={{ margin: 1, borderRadius: 1 }} size="small" className="input">
+                                        <InputLabel>Quantidade</InputLabel>
+                                        <FormInput
+                                            name="Quantidade"
+                                            type="text"
+                                            label="Quantidade"
+                                        />
+                                    </FormControl>
 
-                                        <FormControl size="small" className="input">
-                                            <InputLabel>Id da compania</InputLabel>
-                                            <FormInput
-                                                name="companyId"
-                                                type="text"
-                                                label="Id da compania"
-                                            />
-                                        </FormControl>
+                                    <FormControl sx={{ margin: 1, borderRadius: 1 }} size="small" className="input">
+                                        <InputLabel>Id da compania</InputLabel>
+                                        <FormInput
+                                            name="companyId"
+                                            type="text"
+                                            label="Id da compania"
+                                        />
+                                    </FormControl>
 
 
-                                        <FormControl size="small" className="input">
-                                            <InputLabel>Tamanho</InputLabel>
-                                            <FormInput
-                                                name="Tamanho"
-                                                type="text"
-                                                label="Tamanho"
-                                            />
-                                        </FormControl>
+                                    <FormControl sx={{ margin: 1, borderRadius: 1 }} size="small" className="input">
+                                        <InputLabel>Tamanho</InputLabel>
+                                        <FormInput
+                                            name="Tamanho"
+                                            type="text"
+                                            label="Tamanho"
+                                        />
+                                    </FormControl>
 
-                                        <FormControl size="small" className="input">
-                                            <InputLabel>Cor</InputLabel>
-                                            <FormInput
-                                                name="Cor"
-                                                type="text"
-                                                label="Cor"
-                                            />
-                                        </FormControl>
+                                    <FormControl sx={{ margin: 1, borderRadius: 1 }} size="small" className="input">
+                                        <InputLabel>Cor</InputLabel>
+                                        <FormInput
+                                            name="Cor"
+                                            type="text"
+                                            label="Cor"
+                                        />
+                                    </FormControl>
 
-                                    </Box>
+                                </Box>
 
-                                    <Box  sx={{display:"flex", flexDirection:"column"}}>
-                                        <FormControl size="small" className="input">
-                                            <InputLabel>Preço</InputLabel>
-                                            <FormInput
-                                                name="price"
-                                                type="number"
-                                                label="Preço"
-                                            />
-                                        </FormControl>
+                                <Box sx={{ display: "flex", flexDirection: "column", width:"50%"}}>
+                                    <FormControl sx={{ margin: 1, borderRadius: 1 }} size="small" className="input">
+                                        <InputLabel>Preço</InputLabel>
+                                        <FormInput
+                                            name="price"
+                                            type="number"
+                                            label="Preço"
+                                        />
+                                    </FormControl>
 
-                                        <FormControl size="small" className="input">
-                                            <InputLabel>Id de fornecimento</InputLabel>
-                                            <FormInput
-                                                name="supplierId"
-                                                type="text"
-                                                label="Id de fornecimento"
-                                            />
-                                        </FormControl>
+                                    <FormControl sx={{ margin: 1, borderRadius: 1 }} size="small" className="input">
+                                        <InputLabel>Id de fornecimento</InputLabel>
+                                        <FormInput
+                                            name="supplierId"
+                                            type="text"
+                                            label="Id de fornecimento"
+                                        />
+                                    </FormControl>
 
-                                        <FormControl size="small" className="input">
-                                            <InputLabel>Genero</InputLabel>
-                                            <FormInput
-                                                name="Genero"
-                                                type="text"
-                                                label="Genero"
-                                            />
-                                        </FormControl>
+                                    <FormControl sx={{ margin: 1, borderRadius: 1 }} size="small" className="input">
+                                        <InputLabel>Genero</InputLabel>
+                                        <FormInput
+                                            name="Genero"
+                                            type="text"
+                                            label="Genero"
+                                        />
+                                    </FormControl>
 
-                                        <FormControl size="small" className="input">
-                                            <InputLabel>Tipo</InputLabel>
-                                            <FormInput
-                                                name="Tipo"
-                                                type="text"
-                                                label="Tipo"
-                                            />
-                                        </FormControl>
-                                    </Box>
+                                    <FormControl sx={{ margin: 1, borderRadius: 1 }} size="small" className="input">
+                                        <InputLabel>Tipo</InputLabel>
+                                        <FormInput
+                                            name="Tipo"
+                                            type="text"
+                                            label="Tipo"
+                                        />
+                                    </FormControl>
                                 </Box>
                             </Box>
                         </Box>
