965897b3c49f746eae5f628ebe107f885dfc7c2e
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2022-10-21 14:24:53 -0300

    form cadastro um pouco melhor

--- src/shared/components/forms/Register_Product_Form.tsx
+++ src/shared/components/forms/Register_Product_Form.tsx
@@ -1,124 +1,188 @@
-import { Box, FormControl, InputLabel, Divider, Button } from "@mui/material"
+import { Box, FormControl, InputLabel, Button, Divider } from "@mui/material"
 import { Form } from "@unform/web"
 import { FormInput } from "./input"
 import { useRef } from "react"
 import { FormHandles } from "@unform/core"
 import { IProduct } from "../../service/api"
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+import "./register_Product.css"
 
 export const Register_Product_Form: React.FC<{
-    saveProduct: (e: IProduct)=> void
+    saveProduct: (e: IProduct) => void
 }> = ({
     saveProduct
 }) => {
-    //props form
-    const formRef = useRef<FormHandles>(null)
+        //props form
+        const formRef = useRef<FormHandles>(null)
 
-    return (
-        <div>
+        return (
+
             <Form
                 ref={formRef}
                 className="product-form"
                 onSubmit={(dados) => saveProduct(dados)}
             >
-                <Box sx={{ p: 1, width: "100%" }}>
-                    <FormControl sx={{ width: "100%" }}>
-                        <InputLabel>Nome do Produto</InputLabel>
-                        <FormInput
-                            name="info"
-                            type="text"
-                            label="Nome do Produto"
-                        />
-                    </FormControl>
-                </Box>
-                <FormControl sx={{ p: 1, width: "100%" }}>
-                    <InputLabel>Id de Referência</InputLabel>
-                    <FormInput
-                        name="productReferenceId"
-                        type="text"
-                        label="Id de Referência"
-
-                    />
-                </FormControl>
-                <Box sx={{ p: 1, display: "flex", justifyContent: "space-between", width: "100%" }}>
-                    <FormControl sx={{ width: "49%" }}>
-                        <InputLabel>RFID</InputLabel>
-                        <FormInput
-                            name="codeRFID"
-                            type="text"
-                            label="RFID"
-
-                        />
-                    </FormControl>
-                    <FormControl sx={{ width: "49%" }}>
-                        <InputLabel>Preço</InputLabel>
-                        <FormInput
-                            name="price"
-                            type="number"
-                            label="Preço"
-                        />
-                    </FormControl>
-                </Box>
-                <Box sx={{ p: 1, display: "flex", justifyContent: "space-between", width: "100%" }}>
-                    <FormControl sx={{ width: "49%" }}>
-                        <InputLabel>Id da compania</InputLabel>
-                        <FormInput
-                            name="companyId"
-                            type="text"
-                            label="Id da compania"
-                        />
-                    </FormControl>
-                    <FormControl sx={{ width: "49%" }}>
-                        <InputLabel>Id de fornecimento</InputLabel>
-                        <FormInput
-                            name="supplierId"
-                            type="text"
-                            label="Id de fornecimento"
-                        />
-                    </FormControl>
-                </Box>
-                <Box sx={{ p: 1, width: "100%" }}>
-                    <FormControl sx={{ width: "100%" }}>
-                        <InputLabel>Descrição</InputLabel>
-                        <FormInput
-                            multiline
-                            maxRows={4}
-                            sx={{
-                                height: 130,
-                                display: "flex",
-                                alignItems: "flex-start"
-                            }}
-                            name="description"
-                            type="text"
-                            label="Descrição"
-                        />
-                    </FormControl>
+                <Box className="tittle">
+                    Cadastrar
                 </Box>
                 <Divider flexItem />
-                <Box
-                    sx={{
-                        p: 1,
-                        width: "100%",
-                        display: "flex",
-                        justifyContent: "flex-end",
-                        alignItems: "center"
-                    }}>
-                    <Button
-                        type="submit"
-                        sx={{
-                            width: "100px",
-                            fontSize: "12px",
-                            fontStyle: "normal",
-                            fontWeight: 500,
-                            boxShadow: "none",
-                            borderRadius: 1,
-                            color: "#fff",
-                        }}
-                        variant="contained"
+                <Box className="form_box">
+                    <Box className="container da imagem e form"
+                        bgcolor={"#fff"}
+                        display={"flex"}
+                        flexDirection={"row"}
+                        alignItems={"center"}
                     >
-                        Finalizar
-                    </Button>
+                        <Box className="imagem"
+                            sx={{
+                                minWidth: {
+                                    sm: 100,
+                                    md: 300,
+                                    xl: 300,
+                                },
+                                height: {
+                                    sm: 100,
+                                    md: 300,
+                                    xl: 300,
+                                },
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center",
+                                bgcolor:"#D9D9D9",
+                                margin:6
+                            }}
+                        >
+                            <Button
+                                type="submit"
+                                variant="contained"
+                            >
+                                <UploadFileIcon />Upload
+                            </Button>
+
+                        </Box>
+                        <Box
+                        sx={{
+                            height: {
+                                sm: 100,
+                                md: 300,
+                                xl: 300,
+                            },
+                            width:"100%",
+                        }}
+                        >
+                            <Box className="form"
+                                sx={{
+                                    width:"90%",
+                                    display:"flex",
+                                    flexDirection:"column",
+                                    padding:1
+                                }}
+                                // bgcolor={"#ccc"}
+                            >
+                                <FormControl size="small" className="input">
+                                    <InputLabel>Nome do Produto</InputLabel>
+                                    <FormInput
+                                        name="info"
+                                        type="text"
+                                        label="Nome do Produto"
+                                    />
+                                </FormControl>
+
+                                <Box sx={{display:"flex"}}>
+                                    <Box sx={{display:"flex", flexDirection:"column"}}>
+                                        <FormControl size="small" className="input">
+                                            <InputLabel>Quantidade</InputLabel>
+                                            <FormInput
+                                                name="Quantidade"
+                                                type="text"
+                                                label="Quantidade"
+                                            />
+                                        </FormControl>
+
+                                        <FormControl size="small" className="input">
+                                            <InputLabel>Id da compania</InputLabel>
+                                            <FormInput
+                                                name="companyId"
+                                                type="text"
+                                                label="Id da compania"
+                                            />
+                                        </FormControl>
+
+
+                                        <FormControl size="small" className="input">
+                                            <InputLabel>Tamanho</InputLabel>
+                                            <FormInput
+                                                name="Tamanho"
+                                                type="text"
+                                                label="Tamanho"
+                                            />
+                                        </FormControl>
+
+                                        <FormControl size="small" className="input">
+                                            <InputLabel>Cor</InputLabel>
+                                            <FormInput
+                                                name="Cor"
+                                                type="text"
+                                                label="Cor"
+                                            />
+                                        </FormControl>
+
+                                    </Box>
+
+                                    <Box  sx={{display:"flex", flexDirection:"column"}}>
+                                        <FormControl size="small" className="input">
+                                            <InputLabel>Preço</InputLabel>
+                                            <FormInput
+                                                name="price"
+                                                type="number"
+                                                label="Preço"
+                                            />
+                                        </FormControl>
+
+                                        <FormControl size="small" className="input">
+                                            <InputLabel>Id de fornecimento</InputLabel>
+                                            <FormInput
+                                                name="supplierId"
+                                                type="text"
+                                                label="Id de fornecimento"
+                                            />
+                                        </FormControl>
+
+                                        <FormControl size="small" className="input">
+                                            <InputLabel>Genero</InputLabel>
+                                            <FormInput
+                                                name="Genero"
+                                                type="text"
+                                                label="Genero"
+                                            />
+                                        </FormControl>
+
+                                        <FormControl size="small" className="input">
+                                            <InputLabel>Tipo</InputLabel>
+                                            <FormInput
+                                                name="Tipo"
+                                                type="text"
+                                                label="Tipo"
+                                            />
+                                        </FormControl>
+                                    </Box>
+                                </Box>
+                            </Box>
+                        </Box>
+                    </Box>
+                </Box>
+                <Divider flexItem />
+                <Box className="foot">
+                    <Box p={2}>
+                        <Button
+                            type="submit"
+                            variant="contained"
+                        >
+                            Finalizar
+                        </Button>
+                    </Box>
                 </Box>
             </Form>
-        </div>
-    )
-}
+
+        )
+    }
--- src/shared/components/modal/Product_Modal.tsx
+++ src/shared/components/modal/Product_Modal.tsx
@@ -2,30 +2,29 @@ import { Modal, Box, Dialog, Button } from "@mui/material";
 import { useState } from "react";
 import { Confirm_Dialog } from "../dialog";
 
-interface props{
-    outState: boolean
-    closeModal: ()=>void
+interface props {
+  outState: boolean
+  closeModal: () => void
 }
 //todo componente que usar o modal terá que enviar uma func de close e uma var do estado
-export const Product_Modal: React.FC<props> = ({children, outState, closeModal}) =>{
+export const Product_Modal: React.FC<props> = ({ children, outState, closeModal }) => {
 
-    const [dialog, setDialog] = useState<boolean>(false)
-    
-    return(
-      <>
-        <Modal
+  const [dialog, setDialog] = useState<boolean>(false)
+
+  return (
+    <>
+      <Modal
+        sx={{borderRadius:"20px"}}
         open={outState}
-        onClose={()=>setDialog(true)}
-        >
-          <Box
+        onClose={() => setDialog(true)}
+      >
+        <Box
           sx={{
-            bgcolor:"#000",
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
 
-            display:'flex',
-            alignItems:'center',
-            justifyContent:'center',
-
-            position:'absolute',
+            position: 'absolute',
             top: '50%',
             left: '50%',
             transform: 'translate(-50%, -50%)',
@@ -40,31 +39,19 @@ export const Product_Modal: React.FC<props> = ({children, outState, closeModal})
               md: 600,
               xl: 600,
             },
-            
-            borderRadius:"18px",
-            p: 2
           }}
-          >
-            <Box
-            sx={{
-            width:"100%",
-            height:"100%",
-            display:"flex",
-            flexDirection:"column"
-            }}
-            >
-              {children}
-            </Box>
-          </Box>
-        </Modal>
-        <Confirm_Dialog
+        >
+          {children}
+        </Box>
+      </Modal>
+      <Confirm_Dialog
         messageOne={'Não'}
         messageTwo={'Sim'}
         tittle={'Deseja fechar?'}
         state={dialog}
-        funcDialog={()=>setDialog(false)}
+        funcDialog={() => setDialog(false)}
         funcModal={closeModal}
-        />
-      </>
-    )
+      />
+    </>
+  )
 }
