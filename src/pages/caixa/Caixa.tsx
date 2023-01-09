1087d89be5b7d4648fe1283aff2be56c4d3f6cf7
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-01-09 15:37:50 -0300

    inicio tela de caixa

--- src/pages/caixa/Caixa.tsx
+++ src/pages/caixa/Caixa.tsx
@@ -1,13 +1,34 @@
+import { Typography } from "@mui/material";
 import { ContentLayout } from "../../shared/layout";
 import "./caixa.css";
+import { SearchInput } from "../../shared";
+import { useState } from "react";
 
 export const Caixa = () => {
+
+    const [value, setValue] = useState<string>("");
 
     return (
         <ContentLayout tittle={"Caixa"}>
             
             <div className="container-externo">
-                oisds
+                <div className="container-interno">
+                    <div className="produtos">
+                        <div className="search">
+                            <Typography>Código RFID</Typography>
+                            <SearchInput change={(value) => { setValue(value.target.value);}}/>
+                        </div>
+                        <div className="ultimoProduto">
+                            ultimo produto
+                        </div>
+                        <div className="carrinho">
+                            carrinho
+                        </div>
+                    </div>
+                    <div className="pagamento">
+                        pagamento
+                    </div>
+                </div>
             </div>
             
         </ContentLayout>
